dbbda4e5098206d56fd2d9393d326441cb1f95cf
Denis Pronin <dannftk@yandex.ru>
2025-01-20 23:03:24 +0300

    introduce xroost::memory::unique_ptr
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

---
+++ include/xroost/memory/unique_ptr.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <memory>
+#include <type_traits>
+#include <utility>
+
+#if __cplusplus < 202002UL || !__has_cpp_attribute(no_unique_address)
+#error "cannot compile with [[no_unique_address]] atribute unsupported"
+#endif
+
+namespace xroost::memory {
+
+template <typename T, typename Deleter = std::default_delete<T>>
+class unique_ptr {
+public:
+  unique_ptr() = default;
+  explicit unique_ptr(T *p, Deleter d = Deleter()) noexcept(
+      std::is_nothrow_move_assignable_v<Deleter> ||
+      std::is_nothrow_copy_assignable_v<Deleter>)
+      : p_(p), d_(std::move_if_noexcept(d)) {}
+
+  ~unique_ptr() noexcept(
+      noexcept(std::declval<Deleter>().operator()(static_cast<T *>(nullptr)))) {
+    if (p_)
+      d_(p_);
+  }
+
+  unique_ptr(unique_ptr const &) = delete;
+  unique_ptr &operator=(unique_ptr const &) = delete;
+
+  unique_ptr &operator=(unique_ptr &&other) noexcept(
+      std::is_nothrow_move_assignable_v<Deleter> ||
+      std::is_nothrow_copy_assignable_v<Deleter>) {
+    if (this != &other) {
+      p_ = std::exchange(other.p_, nullptr);
+      d_ = std::move_if_noexcept(other.d_);
+    }
+    return *this;
+  }
+
+  unique_ptr(unique_ptr &&other) noexcept(
+      std::is_nothrow_move_assignable_v<unique_ptr>) {
+    if (this != &other)
+      *this = std::move(other);
+  }
+
+  T *operator->() const { return p_; }
+  T &operator*() const { return *p_; }
+
+  T *get() const { return p_; }
+
+  void reset() {
+    if (p_) {
+      d_(p_);
+      p_ = nullptr;
+    }
+  }
+
+private:
+  T *p_{nullptr};
+  [[no_unique_address]] Deleter d_{};
+};
+
+} // namespace xroost::memory
