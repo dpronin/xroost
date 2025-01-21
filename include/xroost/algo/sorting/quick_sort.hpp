267b0eb615dd2468dc9b4a6fe4ed069c3609b860
Denis Pronin <dannftk@yandex.ru>
2025-01-22 00:20:27 +0300

    update quick_sort and quick_sort_r
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- include/xroost/algo/sorting/quick_sort.hpp
+++ include/xroost/algo/sorting/quick_sort.hpp
@@ -1,46 +1,56 @@
 #include <cstddef>
 
 #include <algorithm>
+#include <concepts>
 #include <functional>
 #include <iterator>
 #include <ranges>
 #include <stack>
+#include <utility>
+
+#include <xroost/algo/partition.hpp>
 
 namespace xroost::algo {
+
+template <std::ranges::random_access_iterator I, std::sentinel_for<I> S,
+          typename Comp = std::ranges::less, typename Proj = std::identity>
+  requires std::sortable<I, Comp, Proj>
+constexpr void quick_sort(I first, S last, Comp comp = {}, Proj proj = {}) {
+  if (std::ranges::distance(first, last) < 2) [[unlikely]]
+    return;
+
+  std::stack<decltype(std::ranges::subrange{first, last})> s;
+  s.push({first, last});
+  while (!s.empty()) {
+    auto const [first, last] = s.top();
+    s.pop();
+    // the middle item swapped for the first to set the latter to be a pivot
+    // item to compare against
+    std::iter_swap(first, first + (last - first) / 2);
+    // partition the range [first + 1, last) with respect to the first item as
+    // the pivot
+    auto mid =
+        partition(first + 1, last,
+                  [=](auto const &v) { return comp(proj(v), proj(*first)); }) -
+        1;
+    // swap the first item for the last item that in the first part of
+    // partitioned range upon this operation the range is devided into [first,
+    // mid - 1] < mid <= [mid + 1, last)
+    std::iter_swap(first, mid);
+    if (mid - first > 1) [[likely]]
+      s.push({first, mid});
+    ++mid;
+    if (last - mid > 1) [[likely]]
+      s.push({mid, last});
+  }
+}
 
 template <std::ranges::random_access_range Range,
           typename Comp = std::ranges::less, typename Proj = std::identity>
   requires std::sortable<std::ranges::iterator_t<Range>, Comp, Proj>
-constexpr std::ranges::borrowed_iterator_t<Range>
-quick_sort(Range &&rng, Comp comp = {}, Proj proj = {}) {
-  if (std::ranges::size(rng) > 1) [[likely]] {
-    std::stack<decltype(std::ranges::subrange{rng})> s;
-    s.push(rng);
-    while (!s.empty()) {
-      auto const [first, last] = s.top();
-      s.pop();
-      // the middle item swapped for the first to set the latter to be a pivot
-      // item to compare against
-      std::iter_swap(first, first + (last - first) / 2);
-      // partition the range [first + 1, last) with respect to the first item as
-      // the pivot
-      auto mid = std::partition(first + 1, last,
-                                [=](auto const &v) {
-                                  return comp(proj(v), proj(*first));
-                                }) -
-                 1;
-      // swap the first item for the last item that in the first part of
-      // partitioned range upon this operation the range is devided into [first,
-      // mid - 1] < mid <= [mid + 1, last)
-      std::iter_swap(first, mid);
-      if (mid - first > 1) [[likely]]
-        s.push({first, mid});
-      ++mid;
-      if (last - mid > 1) [[likely]]
-        s.push({mid, last});
-    }
-  }
-  return std::ranges::end(rng);
+constexpr void quick_sort(Range &&rng, Comp comp = {}, Proj proj = {}) {
+  quick_sort(std::ranges::begin(rng), std::ranges::end(rng), std::move(comp),
+             std::move(proj));
 }
 
 } // namespace xroost::algo
--- include/xroost/algo/sorting/quick_sort_r.hpp
+++ include/xroost/algo/sorting/quick_sort_r.hpp
@@ -1,30 +1,30 @@
 #include <cstddef>
 
 #include <algorithm>
+#include <concepts>
 #include <functional>
 #include <iterator>
 #include <ranges>
+#include <utility>
+
+#include <xroost/algo/partition.hpp>
 
 namespace xroost::algo {
 
-template <std::ranges::random_access_range Range,
+template <std::ranges::random_access_iterator I, std::sentinel_for<I> S,
           typename Comp = std::ranges::less, typename Proj = std::identity>
-  requires std::sortable<std::ranges::iterator_t<Range>, Comp, Proj>
-constexpr std::ranges::borrowed_iterator_t<Range>
-quick_sort_r(Range &&rng, Comp comp = {}, Proj proj = {}) {
-  if (auto const dst{std::ranges::size(rng)}; dst > 1) [[likely]] {
-    auto first{std::ranges::begin(rng)};
-    auto last{std::ranges::end(rng)};
+  requires std::sortable<I, Comp, Proj>
+constexpr void quick_sort_r(I first, S last, Comp comp = {}, Proj proj = {}) {
+  if (auto const dst{std::ranges::distance(first, last)}; dst > 1) [[likely]] {
     // the middle item swapped for the first to set the latter to be a pivot
     // item to compare against
     std::iter_swap(first, first + dst / 2);
     // partition the range [first + 1, last) with respect to the first item as
     // the pivot
-    auto const mid = std::partition(first + 1, last,
-                                    [=](auto const &v) {
-                                      return comp(proj(v), proj(*first));
-                                    }) -
-                     1;
+    auto const mid =
+        partition(first + 1, last,
+                  [=](auto const &v) { return comp(proj(v), proj(*first)); }) -
+        1;
     // swap the first item for the last item that in the first part of
     // partitioned range upon this operation the range is devided into [first,
     // mid - 1] < mid <= [mid + 1, last)
@@ -32,7 +32,14 @@ quick_sort_r(Range &&rng, Comp comp = {}, Proj proj = {}) {
     quick_sort_r(std::ranges::subrange{first, mid}, comp, proj);
     quick_sort_r(std::ranges::subrange{mid + 1, last}, comp, proj);
   }
-  return std::ranges::end(rng);
+}
+
+template <std::ranges::random_access_range Range,
+          typename Comp = std::ranges::less, typename Proj = std::identity>
+  requires std::sortable<std::ranges::iterator_t<Range>, Comp, Proj>
+constexpr void quick_sort_r(Range &&rng, Comp comp = {}, Proj proj = {}) {
+  quick_sort_r(std::ranges::begin(rng), std::ranges::end(rng), std::move(comp),
+               std::move(proj));
 }
 
 } // namespace xroost::algo
