bfe12987947224a297989de0b5ec955626bec530
Denis Pronin <dannftk@yandex.ru>
2025-01-21 22:14:19 +0300

    update avl_tree implementation
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- include/xroost/avl_tree.hpp
+++ include/xroost/avl_tree.hpp
@@ -3,10 +3,13 @@
 #include <sys/types.h>
 
 #include <algorithm>
+#include <functional>
 #include <iostream>
 #include <memory>
 #include <stack>
 #include <utility>
+
+#include <xroost/memory/unique_ptr.hpp>
 
 namespace xroost {
 
@@ -15,7 +18,7 @@ private:
   using value_type = T;
   using allocator_type = Allocator;
 
-  class node {
+  class node final {
     friend class avl_tree<value_type, allocator_type>;
 
   public:
@@ -35,50 +38,59 @@ private:
       return height_ = std::max(l_height, r_height) + 1;
     }
 
-    ssize_t bfactor() const noexcept {
+    ssize_t bfactor() const {
       return (p_left_ ? p_left_->height_ : 0) -
              (p_right_ ? p_right_->height_ : 0);
     }
 
-    value_type const &value() const noexcept { return value_; }
-    size_t height() const noexcept { return height_; }
+    value_type const &value() const { return value_; }
+    size_t height() const { return height_; }
 
   private:
     value_type value_;
     size_t height_{1};
-    std::shared_ptr<node> p_left_, p_right_;
+    memory::unique_ptr<node, std::function<void(struct node *)>> p_left_,
+        p_right_;
   };
 
   class IPrinterStrategy {
   public:
-    IPrinterStrategy() noexcept = default;
-    virtual ~IPrinterStrategy() noexcept = default;
+    IPrinterStrategy() = default;
+    virtual ~IPrinterStrategy() = default;
 
-    virtual void
-    operator()(std::shared_ptr<node> const &pTreeRoot) const noexcept = 0;
+    IPrinterStrategy(IPrinterStrategy const &) = delete;
+    IPrinterStrategy &operator=(IPrinterStrategy const &) = delete;
+
+    IPrinterStrategy(IPrinterStrategy &&) = delete;
+    IPrinterStrategy &operator=(IPrinterStrategy &&) = delete;
+
+    virtual void print(node const &node) = 0;
   };
 
   class PreorderPrinterStrategy : public IPrinterStrategy {
   public:
-    PreorderPrinterStrategy() noexcept = default;
-    ~PreorderPrinterStrategy() noexcept override = default;
+    PreorderPrinterStrategy() = default;
+    ~PreorderPrinterStrategy() override = default;
+
+    PreorderPrinterStrategy(PreorderPrinterStrategy const &) = delete;
+    PreorderPrinterStrategy &
+    operator=(PreorderPrinterStrategy const &) = delete;
+
+    PreorderPrinterStrategy(PreorderPrinterStrategy &&) = delete;
+    PreorderPrinterStrategy &operator=(PreorderPrinterStrategy &&) = delete;
 
   private:
-    void
-    operator()(std::shared_ptr<node> const &p_root) const noexcept override {
-      if (!p_root)
-        return;
-
-      std::stack<std::shared_ptr<node>> nodes;
-      nodes.push(p_root);
+    void print(node const &node) override {
+      std::stack<struct node const *> nodes;
+      nodes.push(&node);
       while (!nodes.empty()) {
         auto p_node{nodes.top()};
         nodes.pop();
         std::cout << p_node->value() << " ";
         if (p_node->p_right_)
-          nodes.push(p_node->p_right_);
+          nodes.push(p_node->p_right_.get());
         if (p_node->p_left_)
-          nodes.push(p_node->p_left_);
+          nodes.push(p_node->p_left_.get());
       }
       std::cout << std::endl;
     }
@@ -89,25 +101,27 @@ private:
     InorderPrinterStrategy() = default;
     ~InorderPrinterStrategy() override = default;
 
-  private:
-    void
-    operator()(std::shared_ptr<node> const &p_root) const noexcept override {
-      if (!p_root)
-        return;
+    InorderPrinterStrategy(InorderPrinterStrategy const &) = delete;
+    InorderPrinterStrategy &operator=(InorderPrinterStrategy const &) = delete;
 
-      std::stack<std::shared_ptr<node>> nodes;
-      auto p_node{p_root};
+    InorderPrinterStrategy(InorderPrinterStrategy &&) = delete;
+    InorderPrinterStrategy &operator=(InorderPrinterStrategy &&) = delete;
+
+  private:
+    void print(node const &node) override {
+      std::stack<struct node const *> nodes;
+      auto *p_node{&node};
       while (p_node || !nodes.empty()) {
         while (p_node) {
           nodes.push(p_node);
-          p_node = p_node->p_left_;
+          p_node = p_node->p_left_.get();
         }
 
         p_node = nodes.top();
         nodes.pop();
         std::cout << p_node->value() << " ";
 
-        p_node = p_node->p_right_;
+        p_node = p_node->p_right_.get();
       }
       std::cout << std::endl;
     }
@@ -115,25 +129,28 @@ private:
 
   class PostorderPrinterStrategy : public IPrinterStrategy {
   public:
-    PostorderPrinterStrategy() noexcept = default;
-    ~PostorderPrinterStrategy() noexcept override = default;
+    PostorderPrinterStrategy() = default;
+    ~PostorderPrinterStrategy() override = default;
+
+    PostorderPrinterStrategy(PostorderPrinterStrategy const &) = delete;
+    PostorderPrinterStrategy &
+    operator=(PostorderPrinterStrategy const &) = delete;
+
+    PostorderPrinterStrategy(PostorderPrinterStrategy &&) = delete;
+    PostorderPrinterStrategy &operator=(PostorderPrinterStrategy &&) = delete;
 
   private:
-    void
-    operator()(std::shared_ptr<node> const &p_root) const noexcept override {
-      if (!p_root)
-        return;
-
-      std::stack<std::shared_ptr<node>> first, second;
-      first.push(p_root);
+    void print(node const &node) override {
+      std::stack<struct node const *> first, second;
+      first.push(&node);
       while (!first.empty()) {
         auto p_node{first.top()};
         first.pop();
         second.push(p_node);
         if (p_node->p_left_)
-          first.push(p_node->p_left_);
+          first.push(p_node->p_left_.get());
         if (p_node->p_right_)
-          first.push(p_node->p_right_);
+          first.push(p_node->p_right_.get());
       }
 
       while (!second.empty()) {
@@ -150,8 +167,7 @@ public:
 
   enum class printer { prefix, infix, postfix };
 
-  std::pair<std::shared_ptr<node>, bool>
-  insert(value_type const &value) noexcept {
+  bool insert(value_type const &value) noexcept {
     return insert_(p_root_, value);
   }
 
@@ -164,21 +180,21 @@ public:
   }
 
   void print() {
-    if (p_printer_)
-      (*p_printer_)(p_root_);
+    if (p_printer_ && p_root_)
+      p_printer_->print(*p_root_);
     std::cout << std::endl;
   }
 
   void set_printer(printer id) {
     switch (id) {
     case printer::prefix:
-      p_printer_ = std::make_unique<PreorderPrinterStrategy>();
+      p_printer_.reset(new PreorderPrinterStrategy{});
       break;
     case printer::infix:
-      p_printer_ = std::make_unique<InorderPrinterStrategy>();
+      p_printer_.reset(new InorderPrinterStrategy{});
       break;
     case printer::postfix:
-      p_printer_ = std::make_unique<PostorderPrinterStrategy>();
+      p_printer_.reset(new PostorderPrinterStrategy{});
       break;
     default:
       break;
@@ -186,21 +202,24 @@ public:
   }
 
 private:
-  void right_rotate_(std::shared_ptr<node> &p_node) {
-    auto q_node{p_node->p_left_};
-    p_node->p_left_ = q_node->p_right_;
-    q_node->p_right_ = p_node;
-    p_node = q_node;
+  void right_rotate_(
+      memory::unique_ptr<node, std::function<void(struct node *)>> &p_node) {
+    auto q_node{std::move(p_node->p_left_)};
+    p_node->p_left_ = std::move(q_node->p_right_);
+    q_node->p_right_ = std::move(p_node);
+    p_node = std::move(q_node);
   }
 
-  void left_rotate_(std::shared_ptr<node> &p_node) {
-    auto q_node{p_node->p_right_};
-    p_node->p_right_ = q_node->p_left_;
-    q_node->p_left_ = p_node;
-    p_node = q_node;
+  void left_rotate_(
+      memory::unique_ptr<node, std::function<void(struct node *)>> &p_node) {
+    auto q_node{std::move(p_node->p_right_)};
+    p_node->p_right_ = std::move(q_node->p_left_);
+    q_node->p_left_ = std::move(p_node);
+    p_node = std::move(q_node);
   }
 
-  void balance_(std::shared_ptr<node> &p_node) {
+  void balance_(
+      memory::unique_ptr<node, std::function<void(struct node *)>> &p_node) {
     p_node->fix_height();
     auto const bfactor{p_node->bfactor()};
     // right subtree is heavier than the left by 2, required to rotate big to
@@ -223,28 +242,30 @@ private:
     }
   }
 
-  std::pair<std::shared_ptr<node>, bool> insert_(std::shared_ptr<node> &p_node,
-                                                 value_type const &value) {
-    std::pair<std::shared_ptr<node>, bool> result;
+  bool
+  insert_(memory::unique_ptr<node, std::function<void(struct node *)>> &p_node,
+          value_type const &value) {
+    bool result{false};
+
     if (!p_node) {
       p_node = {
-              new (allocator_.allocate(1)) node{value},
-              [this](node *p_node) {
-                std::destroy_at(p_node);
-                allocator_.deallocate(p_node, 1);
-              },
-          };
-      result = {p_node, true};
+          new (allocator_.allocate(1)) node{value},
+          [this](node *p_node) {
+            std::destroy_at(p_node);
+            allocator_.deallocate(p_node, 1);
+          },
+      };
+      result = true;
     } else if (value == p_node->value()) {
-      result = std::make_pair(p_node, false);
+      result = false;
     } else if (value < p_node->value()) {
       result = insert_(p_node->p_left_, value);
-      if (result.second)
+      if (result)
         balance_(p_node->p_left_);
       balance_(p_node);
     } else {
       result = insert_(p_node->p_right_, value);
-      if (result.second)
+      if (result)
         balance_(p_node->p_right_);
       balance_(p_node);
     }
@@ -253,8 +274,8 @@ private:
   }
 
 private:
-  std::shared_ptr<node> p_root_;
-  std::unique_ptr<IPrinterStrategy> p_printer_;
+  memory::unique_ptr<node, std::function<void(struct node *)>> p_root_;
+  memory::unique_ptr<IPrinterStrategy> p_printer_;
   std::allocator_traits<allocator_type>::template rebind_alloc<node> allocator_;
 };
 
