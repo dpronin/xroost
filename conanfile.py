e4468b161175b6edbe72a83fb2af741926080034
Denis Pronin <dannftk@yandex.ru>
2025-01-21 21:16:56 +0300

    Version 0.9.1
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import CMake, cmake_layout
 
 class xroostRecipe(ConanFile):
     name = "xroost"
-    version = "0.9.0"
+    version = "0.9.1"
 
     license = "New BSD License"
     author = "Denis Pronin"
