b0e3bc16eaf0ef4b174281e7fc1f4afb02612a32
Denis Pronin <dannftk@yandex.ru>
2025-01-21 23:26:24 +0300

    update bubble_sort
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- include/xroost/algo/sorting/bubble_sort.hpp
+++ include/xroost/algo/sorting/bubble_sort.hpp
@@ -1,23 +1,17 @@
 #include <cstddef>
 
 #include <algorithm>
-#include <functional>
-#include <iostream>
+#include <concepts>
 #include <iterator>
 #include <ranges>
-#include <sstream>
-#include <vector>
+#include <utility>
 
 namespace xroost::algo {
 
-template <std::ranges::random_access_range Range,
+template <std::random_access_iterator I, std::sentinel_for<I> S,
           typename Comp = std::ranges::less, typename Proj = std::identity>
-  requires std::sortable<std::ranges::iterator_t<Range>, Comp, Proj>
-constexpr std::ranges::borrowed_iterator_t<Range>
-bubble_sort(Range &&rng, Comp comp = {}, Proj proj = {}) {
-  auto first{std::ranges::begin(rng)};
-  auto last{std::ranges::end(rng)};
-
+  requires std::sortable<I, Comp, Proj>
+void bubble_sort(I first, S last, Comp comp = {}, Proj proj = {}) {
   for (auto j_last{last - 1}; first < j_last; --j_last) {
     bool swapped{false};
 
@@ -31,8 +25,14 @@ bubble_sort(Range &&rng, Comp comp = {}, Proj proj = {}) {
     if (!swapped)
       break;
   }
+}
 
-  return std::ranges::end(rng);
+template <std::ranges::random_access_range Range,
+          typename Comp = std::ranges::less, typename Proj = std::identity>
+  requires std::sortable<std::ranges::iterator_t<Range>, Comp, Proj>
+constexpr void bubble_sort(Range &&rng, Comp comp = {}, Proj proj = {}) {
+  bubble_sort(std::ranges::begin(rng), std::ranges::end(rng), std::move(comp),
+              std::move(proj));
 }
 
 } // namespace xroost::algo
