afc4ad02db57e8be0adac213a45b7e9029afd9bd
Denis Pronin <dannftk@yandex.ru>
2025-01-21 23:15:15 +0300

    update selection_sort
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- include/xroost/algo/sorting/selection_sort.hpp
+++ include/xroost/algo/sorting/selection_sort.hpp
@@ -1,25 +1,30 @@
 #include <cstddef>
 
 #include <algorithm>
+#include <concepts>
 #include <functional>
 #include <iterator>
 #include <ranges>
+#include <utility>
 
 namespace xroost::algo {
+
+template <std::random_access_iterator I, std::sentinel_for<I> S,
+          typename Comp = std::ranges::less, typename Proj = std::identity>
+  requires std::sortable<I, Comp, Proj>
+constexpr void selection_sort(I first, S last, Comp comp = {},
+                              Proj proj = {}) noexcept {
+  for (; first < last - 1; ++first)
+    std::iter_swap(first, std::ranges::min_element(
+                              std::ranges::subrange{first, last}, comp, proj));
+}
 
 template <std::ranges::random_access_range Range,
           typename Comp = std::ranges::less, typename Proj = std::identity>
   requires std::sortable<std::ranges::iterator_t<Range>, Comp, Proj>
-constexpr std::ranges::borrowed_iterator_t<Range>
-selection_sort(Range &&rng, Comp comp = {}, Proj proj = {}) {
-  auto first{std::ranges::begin(rng)};
-  auto last{std::ranges::end(rng)};
-
-  for (; first < last - 1; ++first)
-    std::iter_swap(first, std::ranges::min_element(
-                              std::ranges::subrange{first, last}, comp, proj));
-
-  return std::ranges::end(rng);
+constexpr void selection_sort(Range &&rng, Comp comp = {}, Proj proj = {}) {
+  selection_sort(std::ranges::begin(rng), std::ranges::end(rng),
+                 std::move(comp), std::move(proj));
 }
 
 } // namespace xroost::algo
