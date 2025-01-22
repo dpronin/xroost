35f18b7afd82b040e8902fd65ecf28bea4f6b433
Denis Pronin <dannftk@yandex.ru>
2025-01-22 20:09:15 +0300

    update insertion_sort
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- include/xroost/algo/sorting/insertion_sort.hpp
+++ include/xroost/algo/sorting/insertion_sort.hpp
@@ -10,18 +10,24 @@
 
 namespace xroost::algo {
 
+template <std::random_access_iterator I, std::sentinel_for<I> S,
+          typename Comp = std::ranges::less, typename Proj = std::identity>
+  requires std::sortable<I, Comp, Proj>
+constexpr void insertion_sort(I first, S last, Comp comp = {}, Proj proj = {}) {
+  for (auto i{first + 1}; i < last; ++i) {
+    std::rotate(std::ranges::upper_bound(std::ranges::subrange{first, i}, *i,
+                                         comp, proj),
+                i, i + 1);
+  }
+}
+
 template <std::ranges::random_access_range Range,
           typename Comp = std::ranges::less, typename Proj = std::identity>
   requires std::sortable<std::ranges::iterator_t<Range>, Comp, Proj>
 constexpr std::ranges::borrowed_iterator_t<Range>
 insertion_sort(Range &&rng, Comp comp = {}, Proj proj = {}) {
-  auto first{std::ranges::begin(rng)};
-  auto last{std::ranges::end(rng)};
-  for (auto i{first + 1}; i < last; ++i)
-    std::rotate(std::ranges::upper_bound(std::ranges::subrange{first, i}, *i,
-                                         comp, proj),
-                i, i + 1);
-  return std::ranges::end(rng);
+  insertion_sort(std::ranges::begin(rng), std::ranges::end(rng),
+                 std::move(comp), std::move(proj));
 }
 
 } // namespace xroost::algo
